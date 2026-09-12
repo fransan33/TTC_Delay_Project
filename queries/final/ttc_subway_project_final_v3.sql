ae619d516ac3a06a6e357e44bc054158856e5e31
Francine Sangil <sangilfrancine@gmail.com>
2026-09-12 16:53:07 -0400

    Update ttc_subway_project_final_v3.sql

--- queries/final/ttc_subway_project_final_v3.sql
+++ queries/final/ttc_subway_project_final_v3.sql
@@ -1,4 +1,4 @@
-/* Final Queries — Production-ready or presentation queries */
+/* Final Queries — queries used for the analyses and dashboards */
 
 /* Skills used: Creating VIEWs, JOINs and CTEs; Aggregate and Window Functions */
 
